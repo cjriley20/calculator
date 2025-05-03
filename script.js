7f1fdff6b0a7ba636b4afcbebfb280718e51eca4
cjriley20 <20461606+cjriley20@users.noreply.github.com>
2025-05-02 22:20:48 -0400

    Add basic functionality

--- script.js
+++ script.js
@@ -24,68 +24,203 @@ function percent(x) {
   return x / 100;
 }
 
-// Mapping from symbols to display text
+// Display
+
+const display = document.querySelector('div.display p')
+
+function updateDisplay(x) {
+  display.textContent = String(x) || '0';
+}
+
+// Calculator state
+
+const Mode = Object.freeze({
+  Start: 'start',
+  FirstOperand: 'firstOperand',
+  Operator: 'operator',
+  SecondOperand: 'secondOperand'
+});
+
+const state = {
+  mode: Mode.Start,
+  operand1: '',
+  operand2: '',
+  operator: null
+}
+
+function resetState() {
+  state.mode = Mode.Start;
+  state.operand1 = '';
+  state.operand2 = '';
+  state.operator = null;
+}
+
+function logState() {
+  console.log('***');
+  console.log(state.mode);
+  console.log(state.operand1);
+  console.log(state.operand2);
+  console.log(state.operator);
+}
+
+// Mapping from operators and functions to display text
 
 const BinaryOperator = Object.freeze({
-  Add: '+',
-  Subtract: '-',
-  Multiply: '*',
-  Divide: '/',
+  Add: { fn: add, label: '+' },
+  Subtract: { fn: subtract, label: '-' },
+  Multiply: { fn: multiply, label: '*' },
+  Divide: { fn: divide, label: '/'} ,
 });
 
 const UnaryOperator = Object.freeze({
-  Negate: '+/-',
-  Percent: '%',
+  Negate: { fn: negate, label: '+/-' },
+  Percent: { fn: percent, label: '%' },
 });
 
 const GeneralFunction = Object.freeze({
-  Clear: 'AC',
-  Equal: '='
+  Clear: { label: 'AC' },
+  Equal: { label: '=' }
 });
 
-// Populate operator and general function buttons.
+// Binary operator buttons
 
 const binaryOperatorButtons = document.querySelectorAll('.binary-operator')
+
 binaryOperatorButtons.forEach((button) => {
   const op = button.dataset.op;
-  button.textContent = BinaryOperator[op];
+
+  button.textContent = BinaryOperator[op].label;
+
+  button.addEventListener('click', (e) => {
+    switch (state.mode) {
+      case Mode.Start:
+        // Start with zero.
+        state.operand1 = '0';
+        state.operator = op;
+        state.mode = Mode.Operator;
+        break;
+      case Mode.FirstOperand:
+        // Finished first operand and waiting for second.
+        state.operator = op;
+        state.mode = Mode.Operator;
+        break;
+      case Mode.Operator:
+        // Overwrite last operator.
+        state.operator = op;
+        break;
+      case Mode.SecondOperand:
+        // Calculate using last operator.
+        let x = Number(state.operand1);
+        let y = Number(state.operand2);
+        let result = BinaryOperator[state.operator].fn(x, y);
+        
+        // Update display.
+        state.operand1 = String(result);
+        updateDisplay(state.operand1);
+
+        state.operator = op;
+        break;
+    }
+  });
 })
+
+// Unary operator buttons
 
 const unaryOperatorButtons = document.querySelectorAll('.unary-operator')
+
 unaryOperatorButtons.forEach((button) => {
   const op = button.dataset.op;
-  button.textContent = UnaryOperator[op];
+
+  button.textContent = UnaryOperator[op].label;
+
+  button.addEventListener('click', (e) => {
+    switch (state.mode) {
+      case Mode.Start:
+        // Operand is zero - nothing to do
+        break;
+      case Mode.FirstOperand: {
+        let x = Number(state.operand1);
+        let result = UnaryOperator[op].fn(x);
+        state.operand1 = String(result);
+        updateDisplay(state.operand1);
+        break;
+      }
+      case Mode.Operator:
+        break;
+      case Mode.SecondOperand: {
+        let x = Number(state.operand2);
+        let result = UnaryOperator[op].fn(x);
+        state.operand2 = String(result);
+        updateDisplay(state.operand2);
+        break;
+      }
+    }
+  });
 })
+
+// General function buttons
 
 const generalFunctionButtons = document.querySelectorAll('.general-function')
+
 generalFunctionButtons.forEach((button) => {
   const op = button.dataset.op;
-  button.textContent = GeneralFunction[op];
+
+  button.textContent = GeneralFunction[op].label;
+
+  button.addEventListener('click', (e) => {
+    switch (GeneralFunction[op]) {
+      case GeneralFunction.Clear:
+        resetState();
+        updateDisplay(state.operand1);
+        break;
+      case GeneralFunction.Equal:
+        switch (state.mode) {
+          case Mode.Start:
+            break;
+          case Mode.FirstOperand:
+            break;
+          case Mode.Operator:
+            break;
+          case Mode.SecondOperand:
+            // Calculate using current operator.
+            let x = Number(state.operand1);
+            let y = Number(state.operand2);
+            let result = BinaryOperator[state.operator].fn(x, y);
+            
+            // Update display.
+            state.operand1 = String(result);
+            updateDisplay(state.operand1);
+            break;
+        }
+        break;
+    }
+  });
 })
 
-// Screen elements
-const display = document.querySelector('div.display p')
+const digitButtons = document.querySelectorAll('.digit')
 
-const buttons = document.querySelectorAll('button.digit')
-
-buttons.forEach((button) => {
+digitButtons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    display.textContent = e.target.textContent;
+    let digit = e.target.textContent;
+    switch (state.mode) {
+      case Mode.Start:
+        state.operand1 = digit;
+        state.mode = Mode.FirstOperand;
+        updateDisplay(state.operand1);
+        break;
+      case Mode.FirstOperand:
+        state.operand1 += digit;
+        updateDisplay(state.operand1);
+        break;
+      case Mode.Operator:
+        state.operand2 = digit;
+        state.mode = Mode.SecondOperand;
+        updateDisplay(state.operand2);
+        break;
+      case Mode.SecondOperand:
+        state.operand2 += digit;
+        updateDisplay(state.operand2);
+        break;
+    }
   });
-});
-  
-// Test
-
-// let askForNumbers = true;
-// while (askForNumbers) {
-//   const x = prompt('Enter x');
-//   const y = prompt('Enter y');
-
-//   if (y == 'q') {
-//     askForNumbers = false;
-//     continue;
-//   }
-
-//   console.log(`x = ${x}`)
-//   console.log(`y = ${y}`)
-// }
+})
